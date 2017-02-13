64538348e8a6c23cb60b57aaa67cb9bef2e9146e
Abhishek kanojia <abhishekka3193@gmail.com>
2017-02-13 20:40:49 +0530

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+	validates_presence_of :title,:description,:image_url,:price
+	validates :title ,length: {minimum: 5, message: "Too short !! atleast 5 characters"}
+	validates :price , numericality:{ greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url,allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
+		message: "must be a URL for gif/png/jpg"}
 end
