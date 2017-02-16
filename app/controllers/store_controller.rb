e6e02bb2f4e6c311d1afd284789fcd08f0e062f9
Abhishek kanojia <abhishekka3193@gmail.com>
2017-02-17 02:55:12 +0530

    Creat destroy cart yet to fix destroy

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,16 @@ class StoreController < ApplicationController
   	time = Time.new
   	@time = time.strftime("%H:%M:%S %p")
   	@product = Product.order(:title)
+
+  	# defining session
+
+  	if session[:count].nil?
+  		session[:count] = 0
+  	else
+  		session[:count]+=1
+  	end
+
+  	@page_visits = session[:count] if session[:count] >= 5
+
   end
 end
