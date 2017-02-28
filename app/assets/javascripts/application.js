a97e660c30880b94fc485c057b734ce6050ec46a
Abhishek kanojia <abhishekka3193@gmail.com>
2017-02-28 06:04:09 +0530

    Iteration I3 complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require jquery-ui/effects/effect-blind
 //= require turbolinks
+//= require bootstrap.min
 //= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :authorize
+	def authorize
+		unless User.find_by(id: session[:user_id])
+			redirect_to login_url, notice: "Please login"
+		end
+	end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
